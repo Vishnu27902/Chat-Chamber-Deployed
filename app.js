a72c86b398ec54d0f20e58a05c3ea33793bc0cb0
Vishnu <106140390+Vishnu27902@users.noreply.github.com>
2023-06-15 14:14:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ const adminRouter = require("./routers/adminRouter")
 const loginRouter = require("./routers/loginRouter")
 const app = express()
 const PORT = process.env.PORT || 5000
-const MONGO_URI = process.env.MONGO_URI
+const MONGO_ATLAS_URI = process.env.MONGO_URI
 
-connectDB(MONGO_URI)
+connectDB(MONGO_ATLAS_URI)
 
 app.use(cors({ origin: true }))
 app.use(session({
