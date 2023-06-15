1b7658daa4a0f769d1ed7361225ecbf13793f5ce
Vishnu <106140390+Vishnu27902@users.noreply.github.com>
2023-06-15 21:15:13 +0530

    Update RoomControllers.js

--- controllers/RoomControllers.js
+++ controllers/RoomControllers.js
@@ -68,7 +68,7 @@ const removeUser = async (req, res) => {
 
 const getUserName = async (req, res) => {
     const { roomID, socketID } = req.query
-    const data = await roomModel.findOne({ _id: roomID, "participants._id": socketID }, { "participants.name": 1 })
+    const data = await roomModel.findOne({ _id: roomID, "participants._id": socketID }, { "participants.name": 1 }).exec()
     console.log(data)
     const username = data.participants.name
     res.status(200).json({ success: true, message: "User Name Fetched Successfully", username })
