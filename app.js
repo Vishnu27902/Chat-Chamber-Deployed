06cfc8a1e9a9286acdc6ff8269aef7b63f90a16d
Vishnu <106140390+Vishnu27902@users.noreply.github.com>
2023-06-15 18:04:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ io.on("connection", (socket) => {
 
     socket.on("disconnect", async (message) => {
         (async () => await axios.delete(`https://chat-chamber-app.onrender.com/home/removeuser?userID=${socket.id}&roomID=${roomID}`))()
-        const {username}=await axios.get(`https://chat-chamber-app.onrender.com/home/removeuser?userID=${socket.id}&roomID=${roomID}`)
+        const {username}=await axios.get(`https://chat-chamber-app.onrender.com/home/getusernamer?userID=${socket.id}&roomID=${roomID}`)
         io.to(roomID).emit("message", messageFormatter(BOT, `${username} left the room`))
     })
 })
